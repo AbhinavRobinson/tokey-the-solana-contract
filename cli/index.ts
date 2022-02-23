14e2a3fd53ba6ecba1021c61ab59c681bfc33a5f
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-02-23 22:44:03 +0530

    init(app): add signer

--- cli/index.ts
+++ cli/index.ts
@@ -1,5 +1,5 @@
 import web3 = require('@solana/web3.js')
-import { PRIVATE_KEY } from './config'
+import { PRIVATE_KEY } from './env'
 
 const key = Uint8Array.from(PRIVATE_KEY)
 const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
