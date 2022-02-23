5ad0a2f5b73411af7e85eebcaff599d2b240343f
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-02-23 22:10:38 +0530

    init(cli): add cli app

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+};
 
+entrypoint!(process_instruction);
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let key: u8 = instruction_data[0];
+    match key {
+        0 => msg!("Hello, world!"),
+        1 => msg!("Hello, Solana!"),
+        _ => msg!("Invalid instruction key"),
+    };
+    Ok(())
+}
